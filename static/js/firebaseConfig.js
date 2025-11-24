603e3aaea6fc34134387c0bca00d2965111b317f
Luís Filipe Medeiros <lfmedeiros.rj@gmail.com>
2025-11-24 12:49:35 -0300

    templates e rotas ok

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhcLHu2pDRGuyWK_4iAfpuaZkiaXDXO7E",
+  authDomain: "py-world-99606.firebaseapp.com",
+  projectId: "py-world-99606",
+  storageBucket: "py-world-99606.firebasestorage.app",
+  messagingSenderId: "500804215434",
+  appId: "1:500804215434:web:713a3ab12f1a43d6f64373"
+};
